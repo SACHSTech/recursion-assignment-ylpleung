ec0fe98de4e96422972508b15258c065c8956f54
Yan Laam Phoebe Leung <yanlaamphoebe.leung21@ycdsbk12.ca>
2020-12-08 20:23:08 +0000

    test count7

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,25 +1,32 @@
 package recursionAssignment;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import recursionAssignment.Recursion;
 import java.io.*;
-
 public class RecursionTest{
     
     //Before
     /**
      * An initial test
      */
-    //Test
+    @Test
+    //success
     public void Test1(){
-        // count7(integer, n)
-        int n;
-        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        assertEquals(1, Recursion.count7(89735));
+    }
 
-        System.out.println("enter number");
-        n = Integer.parseInt(keyboard.readLine());
-        count7(n);
+    @Test
+    //fail
+    public void Test2(){
+      assertEquals(0, Recursion.count7(3247235));
     }
     
+    @Test
+    public void Test3(){
+      assertEquals(-1, Recursion.count7(42874));
+    }
+
     // add more tests
     
     //After
